f835a4f535d36286aea8ad654e076c82271d4de8
Victor Login <batazor111@gmail.com>
2019-03-19 12:19:31 +0300

    fix go release

--- golang/release/build.sh
+++ golang/release/build.sh
@@ -3,10 +3,11 @@
 set -eux
 
 PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
+PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
 
 mkdir -p $PROJECT_ROOT
 rmdir $PROJECT_ROOT
 ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
 cd $PROJECT_ROOT
 go get -v ./...
-go build cmd/main.go
+go build -o $PROJECT_NAME cmd/main.go
